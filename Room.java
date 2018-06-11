c8f8342e679c423cf93b8b0c3424d0b1b889a126
Bruno Armanelli <brunoarmanelli@me.com>
2018-06-11 03:45:12 -0300

    First Commit

---
+++ Room.java
@@ -0,0 +1,14 @@
+class Room {
+    int roomID; // Key
+    int hostID;
+    String roomType;
+    String country;
+    String city;
+    String neighborhood;
+    int reviews;
+    double overallSatisfaction;
+    int accommodates;    
+    double bedrooms;
+    double price;
+    String propertyType;
+}
---
+++ SortingPerformance.java
@@ -0,0 +1,5 @@
+public class SortingPerformance {
+    public static void main(String[] args) {
+
+    }
+}
