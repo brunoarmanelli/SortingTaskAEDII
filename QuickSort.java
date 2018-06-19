0dd7331e9adecf5ccaa544a6307194f02de12bde
Bruno Armanelli <brunoarmanelli@me.com>
2018-06-19 00:43:55 -0300

    É tanta coisa que já nem sei

--- QuickSort.java
+++ QuickSort.java
@@ -1,55 +1,64 @@
 class QuickSort {
-      Counter counter;
-      long startTime, endTime;
+    Counter counter;
+    long startTime, endTime;
 
-      public QuickSort() {
-            this.counter = new Counter();
-      }
+    public QuickSort() {
+        this.counter = new Counter();
+    }
 
-      public QuickSort(Counter counter) {
-            this.counter = counter;
-      }
+    public QuickSort(Counter counter) {
+        this.counter = counter;
+    }
 
-      public Counter sort(Room[] allData) {
-            this.startTime = System.currentTimeMillis();
-    
-            runSort(allData, 0, allData.length - 1);
-    
-            endTime = System.currentTimeMillis();
-            counter.time = endTime - startTime;
-    
-            return this.counter;
-      }
+    public Counter sort(Room[] allData) {
+        this.startTime = System.currentTimeMillis();
 
-      public void runSort(Room[] allData, int inicio, int fim) {
-            if (inicio < fim) {
-                  int posicaoPivo = separar(allData, inicio, fim);
-                  runSort(allData, inicio, posicaoPivo - 1);
-                  runSort(allData, posicaoPivo + 1, fim);
+        runSort(allData, 0, (allData.length - 1));
+
+        endTime = System.currentTimeMillis();
+        counter.time = endTime - startTime;
+
+        return this.counter;
+    }
+
+    void runSort(Room[] arr, int low, int high) {
+        if (arr == null || arr.length == 0) 
+            return;
+        if (low >= high)
+            return;
+
+        // Pick Pivot
+        int middle = low + (high - low) / 2;
+        Room pivot = arr[middle];
+
+        // Make left < pivot and right > pivot
+        int i = low, j = high;
+        while (i <= j) {
+            while(arr[i].roomID < pivot.roomID) {
+                i++;
             }
-      }
-
-      private int separar(Room[] allData, int inicio, int fim) {
-            int pivo = allData[inicio].roomID;
-            int i = inicio + 1, f = fim;
-            while (i <= f) {
-                  counter.comparisons++;
-                  if (allData[i].roomID <= pivo) {
-                        i++;
-                  } else if (pivo < allData[f].roomID) {
-                        counter.comparisons++;
-                        f--;
-                  } else {
-                        counter.swaps++;
-                        int troca = allData[i].roomID;
-                        allData[i].roomID = allData[f].roomID;
-                        allData[f].roomID = troca;
-                        i++;
-                        f--;
-                  }
+            while(arr[j].roomID > pivot.roomID) {
+                j--;
             }
-            allData[inicio].roomID = allData[f].roomID;
-            allData[f].roomID = pivo;
-            return f;
-      }
+
+            this.counter.comparisons++;
+            if (i <= j) {
+                this.counter.swaps++;
+                Room temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        // Sort two subparts
+        if(low < j) {
+            runSort(arr, low, j);
+        }
+
+        if(high > i) {
+            runSort(arr, i, high);
+        }
+    }
 }
--- StatsIO.java
+++ StatsIO.java
@@ -4,20 +4,51 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.io.File;
 
 class StatsIO {
 
     public static void write(Counter results) {
-        String[] sortingTypes = {"Random", "Ascending", "Descending"};
-        String[] sortingMethods = {"BubbleSort", "InsertionSort", "MergeSort", "PatrunoSort", "QuickSort", "SelectionSort"};
+        // Make files for each ordering method and type of sorting order
+        try (FileWriter fileWriter = new FileWriter("results/" + SortingPerformance.RESULTS_FOLDER + "/" + results.getMethod() + results.getType() + ".txt", true)) {
 
-        try (FileWriter fileWriter = new FileWriter("results/" + SortingPerformance.RESULTS_FOLDER + "/" + sortingMethods[results.sortingMethod] + sortingTypes[results.typeOfSort] + ".txt", true)) {
             PrintWriter printWriter = new PrintWriter(fileWriter);
 
-            printWriter.print(sortingTypes[results.typeOfSort] + "\t");
+            printWriter.print(results.getType() + "\t");
             printWriter.print(results.lineLimit + "\t");
             printWriter.print(results.swaps + "\t");
             printWriter.print(results.comparisons + "\t");
+            printWriter.print(results.time + "\n");
+            
+            printWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Make a .csv file with all results
+        
+        File fullResults = new File("results/" + SortingPerformance.RESULTS_FOLDER + "/" + "FullResults.csv");
+        Boolean fullFileExists = fullResults.exists();
+
+        try (FileWriter fileWriter = new FileWriter("results/" + SortingPerformance.RESULTS_FOLDER + "/" + "FullResults.csv", true)) {
+
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+
+            if(!fullFileExists) {
+                printWriter.print("Method; ");
+                printWriter.print("SotingType; ");
+                printWriter.print("LineLimit; ");
+                printWriter.print("Swaps; ");
+                printWriter.print("Comparisons; ");
+                printWriter.print("Time" + "\n");
+            }
+
+            printWriter.print(results.getMethod() + "; ");
+            printWriter.print(results.getType() + "; ");
+            printWriter.print(results.lineLimit + "; ");
+            printWriter.print(results.swaps + "; ");
+            printWriter.print(results.comparisons + "; ");
             printWriter.print(results.time + "\n");
             
             printWriter.close();
